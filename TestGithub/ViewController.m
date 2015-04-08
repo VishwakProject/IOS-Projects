5af73aaf091915aa68751ebfb089ab35a4f56369
VishwakProjects <JannathBegum@Venkats-MacBook-Pro-364.local>
2015-04-08 12:48:30 +0530

    First change

--- TestGithub/ViewController.m
+++ TestGithub/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"Jannath");
+    NSLog(@"Success");
     
     // Do any additional setup after loading the view, typically from a nib.
 }
