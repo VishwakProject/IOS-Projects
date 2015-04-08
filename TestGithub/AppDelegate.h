aa2d3def940130b5b5fa89f3201e9ec3eea7b4de
VishwakProjects <JannathBegum@Venkats-MacBook-Pro-364.local>
2015-04-08 12:37:27 +0530

    First Try
    First Commit

---
+++ TestGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGithub
+//
+//  Created by Jannath Begum on 4/8/15.
+//  Copyright (c) 2015 Vishwak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGithub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGithub
+//
+//  Created by Jannath Begum on 4/8/15.
+//  Copyright (c) 2015 Vishwak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
